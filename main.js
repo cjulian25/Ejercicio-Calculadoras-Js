a94983d1fcb009932ba5099efa684f886e005173
cjulian25 <carrillojulian@hotmail.com>
2023-04-24 17:05:51 -0500

    CalcV2

--- main.js
+++ main.js
@@ -1,51 +1,71 @@
 const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
 const pantallaSuperior = document.getElementById('pantallaSuperior');
 const operando = document.getElementById('operando');
 const calculadora = document.querySelector('.contenedor__calculadora');
 
 let inputSeleccionado = null;
+let num2;
+
+num1.disabled = false;
 
 num1.addEventListener('click', function() {
     inputSeleccionado = num1;
 })
 
-num2.addEventListener('click', function() {
-    inputSeleccionado = num2;
-})
-
 calculadora.addEventListener(`click`, function(evento) {
+    pantallaSuperior.textContent = "";
     if (evento.target.classList.contains('numeros')) {
         if (inputSeleccionado) {
             inputSeleccionado.value += evento.target.textContent;
         }
-    } else if (evento.target.id == "boton--sumar") {
+    } else
+    if (evento.target.id == "boton--sumar") {
         operando.textContent = "+";
+        num2 = num1.value;
+        num1.value = "";
+        num1.disabled = true;
+        console.log(Number(num1.value))
     } else if (evento.target.id == "boton--restar") {
         operando.textContent = "-";
+        num2 = num1.value;
+        num1.value = "";
+        num1.disabled = true;
     } else if (evento.target.id == "boton--multiplicar") {
         operando.textContent = "*";
+        num2 = num1.value;
+        num1.value = "";
+        num1.disabled = true;
     } else if (evento.target.id == "boton--dividir") {
         operando.textContent = "/";
+        num2 = num1.value;
+        num1.value = "";
+        num1.disabled = true;
     } else if (evento.target.id == 'boton--C') {
         num1.value = "";
-        num2.value = "";
+        num2 = "";
         pantallaSuperior.textContent = "";
         operando.textContent = "";
+        num1.disabled = false;
     } else if (evento.target.id = "boton--igual") {
         if (operando.textContent == "+") {
-            pantallaSuperior.textContent = Number(num1.value) + Number(num2.value);
+            pantallaSuperior.textContent = Number(num1.value) + Number(num2);
+            num2 = "";
         } else if (operando.textContent == "-") {
-            pantallaSuperior.textContent = Number(num1.value) - Number(num2.value);
+            pantallaSuperior.textContent = Number(num1.value) - Number(num2);
+            num2 = "";
         } else if (operando.textContent == "*") {
-            pantallaSuperior.textContent = Number(num1.value) * Number(num2.value);
+            pantallaSuperior.textContent = Number(num1.value) * Number(num2);
+            num2 = "";
         } else if (operando.textContent == "/") {
-            if ((num1.value != "") & (num2.value == "")) {
+            if ((num2 != "") & (num1.value == "")) {
                 pantallaSuperior.textContent = `Debes ingresar un divisor`;
-            } else if ((num1.value != "" || num1.value == "") & (num2.value == 0)) {
+            } else if ((num2 == "") & (num1.value != "")) {
+                pantallaSuperior.textContent = `0`;
+            } else if ((num2 != "") & (num1.value == 0)) {
                 pantallaSuperior.textContent = `No se puede dividir entre 0`;
-            } else if ((num1.value != "") & (num2.value != "")) {
-                pantallaSuperior.textContent = Number(num1.value) / Number(num2.value);
+            } else if ((num1.value != "") & (num2 != "")) {
+                pantallaSuperior.textContent = Number(num2) / Number(num1.value);
+                num2 = "";
             }
         }
     }
